3d2b7624ffe1e9ad7748690d4ca28dbcdf8a4899
kolzsticks <134326087+Kolzstikcs@users.noreply.github.com>
2024-04-03 19:57:25 +0100

    Update controller.js

--- Ctrl/controller.js
+++ Ctrl/controller.js
@@ -1,1 +1,34 @@
+const video_hrefs = [
+    "https://kolzstikcs.github.io/h4y12/hy1/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/hy3/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/hy4/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/hy5/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/index.m3u8"
+    ];
 
+    
+     const title_array = [
+      'INTRODUCTION TO HTML',
+      'CSS BASICS',
+      'CSS FEATURES',
+      'HOVER TRANSITION & SHADOW',  
+      'TEXT STYLES',
+      'STRUCTURE OF A WEBPAGE',
+      'IMAGES',
+      'INPUTS & TEXTBOXES',
+      'PROJECT_1- FOOD WEBSITE'
+    ];
+
+    const background_images_url = [
+      'back_img/l1_mobile.webp',
+      'back_img/l2_mobile.webp',
+      'back_img/l3_mobile.webp',
+      'back_img/l4_mobile.webp',
+      'back_img/l5_mobile.webp',
+      'back_img/l6_mobile.webp',
+      'back_img/l7_mobile.webp',
+      'back_img/l8_mobile.webp',
+      'back_img/l9_mobile.webp',
+      'back_img/l7_mobile.webp'
+    ];
