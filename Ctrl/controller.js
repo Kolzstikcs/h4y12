83f9fd4b165ef0a89f50be6a2a176a5454d03834
kolzsticks <134326087+Kolzstikcs@users.noreply.github.com>
2024-04-22 12:40:21 +0100

    Update controller.js

--- Ctrl/controller.js
+++ Ctrl/controller.js
@@ -2,9 +2,9 @@ const video_hrefs = [
     "https://kolzstikcs.github.io/h4y12/hy1/index.m3u8",
     "https://kolzstikcs.github.io/h4y12/index.m3u8",
     "https://kolzstikcs.github.io/h4y12/hy3/index.m3u8",
-    "https://kolzstikcs.github.io/h4y12/hy4/index.m3u8",
-    "https://kolzstikcs.github.io/h4y12/hy5/index.m3u8",
-    "https://kolzstikcs.github.io/h4y12/index.m3u8"
+    "https://kolzstikcs.github.io/h4y12/hy4/index.m3u8"
+    /*"https://kolzstikcs.github.io/h4y12/hy5/index.m3u8",
+    "https://kolzstikcs.github.io/h4y12/index.m3u8"*/
     ];
 
     
@@ -12,22 +12,22 @@ const video_hrefs = [
       'INTRODUCTION TO HTML',
       'CSS BASICS',
       'CSS FEATURES',
-      'HOVER TRANSITION & SHADOW',  
-      'TEXT STYLES',
+      'HOVER TRANSITION & SHADOW' 
+     /* 'TEXT STYLES',
       'STRUCTURE OF A WEBPAGE',
       'IMAGES',
       'INPUTS & TEXTBOXES',
-      'PROJECT_1- FOOD WEBSITE'
+      'PROJECT_1- FOOD WEBSITE'*/
     ];
 
     const background_images_url = [
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l1_mobile.webp',
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l2_mobile.webp',
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l3_mobile.webp',
-      'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l4_mobile.webp',
-      'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l5_mobile.webp',
+      'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l4_mobile.webp'
+      /*'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l5_mobile.webp',
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l6_mobile.webp',
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l7_mobile.webp',
       'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l8_mobile.webp',
-      'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l9_mobile.webp'
+      'https://kolzstikcs.github.io/h4y12/Ctrl/back_img/l9_mobile.webp'*/
     ];
